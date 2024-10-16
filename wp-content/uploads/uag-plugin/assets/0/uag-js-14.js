7e09ad1bbe00702de8fc3ffe39c426d485129ae5
Cocacota <julian.tolosa@est.fi.uncoma.edu.ar>
2024-10-15 21:51:56 -0300

    estilos y usuarios

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-14.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-35744bd0' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-009ff67f' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-4d25fb43 .uagb-swiper",
+		{"autoplay":false,"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-4d25fb43 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-4d25fb43 .swiper-button-next","prevEl":".uagb-block-4d25fb43 .swiper-button-prev"}}	);
+});
+
+ });
--- wp-content/uploads/uag-plugin/assets/0/uag-js-19.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	var swiper = new Swiper( ".uagb-block-12f2957b .uagb-swiper",
-		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-12f2957b .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-12f2957b .swiper-button-next","prevEl":".uagb-block-12f2957b .swiper-button-prev"}}	);
-});
-
- });
